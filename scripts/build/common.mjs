1d588e7903ece18629f3dcc0b4a8ddeca709ee85
Sonny <nidoworkreal@gmail.com>
2026-06-07 09:37:49 +0200

    Update common.mjs

--- scripts/build/common.mjs
+++ scripts/build/common.mjs
@@ -36,7 +36,7 @@ if (!IS_COMPANION_TEST && process.argv.includes("--companion-test"))
     console.error("--companion-test must be run with --reporter for any effect");
 
 export const IS_UPDATER_DISABLED = process.argv.includes("--disable-updater");
-export const gitHash = process.env.MALLCORD_HASH || "PRIV-BETA";
+export const gitHash = process.env.MALLCORD_HASH || "Priv";
 
 export const banner = {
     js: `
